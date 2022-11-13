03a59cfb3a490075fdf5b715cdb556729ad22f9d
xiaoyanh <xiaoyanhn@gmail.com>
2022-11-13 16:50:28 -0500

    res50

--- rkd_app.py
+++ rkd_app.py
@@ -23,7 +23,7 @@ with open('ind_to_label.json') as f:
 # create dictionary
 info = {
     'checkpoint': 'https://github.com/xiaoyanh/RKDapp/blob/main/dataset=RKDMuseum-net=ResNet50-lr=0p01-examples_per_class=None-num_classes=775-train_seed=0-forward_class=Classification-epoch=300.pth?raw=true',
-    'net': 'ResNet18',
+    'net': 'ResNet50',
     'num_classes': len(ind_to_label.keys()),
     'resnet_type': 'big',
     'pretrained': False,
